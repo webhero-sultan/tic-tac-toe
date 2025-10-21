666b83439d7fe3a0150e363d2c71d693d899e752
sultan <webhero.sultan@gmail.com>
2025-10-21 16:09:25 +0600

    moved click function from squares to board component

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,23 +1,30 @@
+import { useState } from "react";
 import Square from "./Square";
 
 
 export default function Board() {
+  const [squares, setSquares] = useState([]);
+  function handleClick(i) {
+    const nextSquares = squares.slice(); // clone squares =====
+    nextSquares[i] = 'X'; // set the value of index 0 =====
+    setSquares(nextSquares); // update value =====
+  }
   return (
     <>
       <div className="flex">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value={squares[0]} onSquareClick={()=> handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick={()=> handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={()=> handleClick(2)}/>
       </div>
       <div className="flex">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value={squares[3]} onSquareClick={()=> handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={()=> handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={()=> handleClick(5)}/>
       </div>
       <div className="flex">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value={squares[6]} onSquareClick={()=> handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={()=> handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={()=> handleClick(8)}/>
       </div>
     </>
   )
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react"
+// import { useState } from "react"
 
-export default function Square() {
-  const [value, setValue] = useState(null);
-  function handleClick () {
-    setValue('X');
-  }
+export default function Square({value, onSquareClick}) {
+  // const [value, setValue] = useState(null);
+  // function handleClick () {
+  //   setValue('X');
+  // }
   return (
-    <button onClick={handleClick} className="h-20 w-20 border-2 text-white text-5xl cursor-pointer glow-blue border-white glow-border rounded-2xl">{value}</button>
+    <button onClick={onSquareClick} className="h-20 w-20 border-2 text-white text-5xl cursor-pointer glow-blue border-white glow-border rounded-2xl">{value}</button>
   )
 }
